5825b290611f70ac4485d878dcca438f343a7dd3
muhammadidris1945 <mi2774979@gmail.com>
2026-09-22 08:10:40 +0700

    Update Biodata.java

--- Biodata.java
+++ Biodata.java
@@ -1,1 +1,10 @@
-rff
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Idris");
+        System.out.println("NIM : 21301004");
+       System.out.println("Program Studi : Sistem Komputer"); 
+        System.out.println("Fakultas : Sains dan Teknologi");
+        System.out.println("Semester : 1");
+    }
+}
